df1f6ca72d503fb068a4c418496bb1f37bbdb889
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-02-28 23:56:07 +0800

    Added election type, program, and year level

---
+++ database/migrations/2022_02_28_155106_add_fields_to_admins_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+            $table->boolean('is_super_admin')->default(false);
+            $table->boolean('is_active')->default(true);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('admins', function (Blueprint $table) {
+        //
+        });
+    }
+};

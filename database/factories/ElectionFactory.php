c5e8a67941a71528b2adf63b715c40a3e516c41b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-03-03 09:53:51 +0800

    Updated ElectionFactory.php

--- database/factories/ElectionFactory.php
+++ database/factories/ElectionFactory.php
@@ -16,8 +16,8 @@ class ElectionFactory extends Factory
         return [
             'title' => $this->faker->word(),
             'description' => $this->faker->text(),
-            'start_at' => Carbon::now(),
-            'end_at' => Carbon::now(),
+            'start_at' => Carbon::now()->addMinute(),
+            'end_at' => Carbon::now()->addDays(3),
             'created_at' => Carbon::now(),
             'updated_at' => Carbon::now(),
             'election_type_id' => $this->faker->randomElement(ElectionType::all())

5a70932f5e00994ed6ee8f663c03bf3cca258100
Dandreb Sinto <dandrebsinto26@gmail.com>
2022-03-08 17:25:15 +0800

    create navigator of active election

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,14 +15,10 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
-
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-nav-link :href="route('active-elections')" :active="request()->routeIs('active-elections')">
                         {{ __('Active Election') }}
                     </x-nav-link>
                 </div>
-
             </div>
 
             <!-- Settings Dropdown -->
@@ -73,9 +69,12 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-        <div class="pt-2 pb-3 space-y-1">
+        <div class="pt-2 pb-3 space-y-1 bg-green-100" >
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('active-elections')" :active="request()->routeIs('active-elections')">
+                {{ __('Active Elections') }}
             </x-responsive-nav-link>
         </div>
 

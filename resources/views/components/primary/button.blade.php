60dc128dbff9e73259f64125ad801b6e36288c29
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-03-15 20:39:37 +0800

    Added button components

---
+++ resources/views/components/primary/button.blade.php
@@ -0,0 +1,3 @@
+<button {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-primary-500 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-700 active:bg-primary-500 focus:outline-none focus:border-primary-500 focus:ring ring-primary-200 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    {{ $slot }}
+</button>

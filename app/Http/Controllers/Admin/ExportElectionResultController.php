73c3821e916f3cfca63dd61bbd2f113fc814d260
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-03-19 18:06:32 +0800

    Added download excel functionality

--- app/Http/Controllers/Admin/ExportElectionResultController.php
+++ app/Http/Controllers/Admin/ExportElectionResultController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers\Admin;
 
+use App\Exports\ExcelElectionResultExport;
 use App\Http\Controllers\Controller;
 use App\Models\Election;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class ExportElectionResultController extends Controller
 {
-    public function store(Request $request, Election $election)
+    public function store(Election $election)
     {
-        // TODO: Export Excel
+        return (new ExcelElectionResultExport($election))->download(str($election->title.'_'.now()->toDateString().'.xlsx')->snake());
     }
 }

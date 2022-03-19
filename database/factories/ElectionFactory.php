f3e8b0d1f223fcbc09b5d05a76f7b5a87ec9a82a
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-03-19 21:46:52 +0800

    Update ElectionFactory.php

--- database/factories/ElectionFactory.php
+++ database/factories/ElectionFactory.php
@@ -19,10 +19,10 @@ class ElectionFactory extends Factory
             'description' => $this->faker->text(),
             'start_at' => Carbon::now(),
             'end_at' => Carbon::now()->addDays(3),
-            'created_at' => Carbon::now(),
-            'updated_at' => Carbon::now(),
             'election_type_id' => ElectionType::whereName('DSG')->firstOrFail(),
             'department_id' => $this->faker->randomElement(Department::all()),
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
         ];
     }
 

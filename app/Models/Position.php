f3ba7324c6702a6283a7c5e89d1194ee99404104
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-03-30 02:02:57 +0800

    Added scopeOfElection

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -2,15 +2,22 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Position extends Model
 {
     use HasFactory;
 
-    public function electionType()
+    public function electionType(): BelongsToMany
     {
         return $this->belongsToMany(ElectionType::class);
+    }
+
+    public function scopeCdsgElection(Builder $query)
+    {
+        $query->whereRelation('electionType', 'id', ElectionType::TYPE_CDSG);
     }
 }

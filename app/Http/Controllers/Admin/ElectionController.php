7123460204fbb2c30a0cfed877d61a2f2fa8c3d8
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-03-29 23:36:44 +0800

    Remove docstring

--- app/Http/Controllers/Admin/ElectionController.php
+++ app/Http/Controllers/Admin/ElectionController.php
@@ -12,33 +12,13 @@ use function view;
 
 class ElectionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
+
     public function index()
     {
         $elections = Election::with(['electionType', 'department'])->latest()->paginate(10);
         return view('admin.elections.index', compact('elections'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
-    public function create()
-    {
-        return view('admin.elections.create');
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param \App\Http\Requests\Admin\StoreElectionRequest $request
-     * @return \Illuminate\Http\RedirectResponse
-     */
     public function store(StoreElectionRequest $request)
     {
         $election = Election::create($request->validated());
@@ -46,49 +26,9 @@ class ElectionController extends Controller
         return redirect()->route('admin.elections.show', compact('election'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param \App\Models\Election $election
-     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
-     */
     public function show(Election $election)
     {
         $positions = $election->loadMissing('electionType')->electionType->positions;
         return view('admin.elections.show', compact('election', 'positions'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param \App\Models\Election $election
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Election $election)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param \App\Http\Requests\Admin\UpdateElectionRequest $request
-     * @param \App\Models\Election $election
-     * @return \Illuminate\Http\Response
-     */
-    public function update(UpdateElectionRequest $request, Election $election)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param \App\Models\Election $election
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Election $election)
-    {
-        //
     }
 }

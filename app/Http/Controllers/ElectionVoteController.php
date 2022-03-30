c95a606beadb3ff2c350cc1361e438e98fe3a192
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-03-31 00:30:30 +0800

    Replace and use ElectionService canVote method

--- app/Http/Controllers/ElectionVoteController.php
+++ app/Http/Controllers/ElectionVoteController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Election;
 use App\Models\User;
 use App\Models\Vote;
+use App\Services\ElectionService;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 use function abort_if;
@@ -16,15 +17,9 @@ class ElectionVoteController extends Controller
 {
     public function create(Request $request, Election $election)
     {
-        $user = $request->user();
+        $canVote = (new ElectionService($election))->canVote($request->user());
 
-        $hasVote = Vote::where('election_id', $election->id)
-            ->where('user_id', $user->id)
-            ->exists();
-
-        $electionEnded = $election->end_at < now();
-
-        abort_if($electionEnded || $hasVote, 403);
+        abort_if(!$canVote, 403);
 
         $positions = $election->electionType->positions;
 
@@ -41,15 +36,8 @@ class ElectionVoteController extends Controller
 
     public function store(Request $request, Election $election)
     {
-        $user = $request->user();
-
-        $hasVote = Vote::where('election_id', $election->id)
-            ->where('user_id', $user->id)
-            ->exists();
-
-        $electionEnded = $election->end_at < now();
-
-        abort_if($electionEnded || $hasVote, 403);
+        $canVote = (new ElectionService($election))->canVote($request->user());
+        abort_if($canVote, 403);
 
         $validator = Validator::make($request->all(), [
             'candidates.*' => 'required|integer',
@@ -58,7 +46,7 @@ class ElectionVoteController extends Controller
         $validated = $validator->validated();
 
         $vote = Vote::create([
-            'user_id' => $user->id,
+            'user_id' => $request->user()->id,
             'election_id' => $election->id,
         ]);
 

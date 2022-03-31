a93211dcbc27de7b13c22b08e97dfd49659927b7
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-01 01:17:23 +0800

    Update ElectionController.php

--- app/Http/Controllers/Admin/ElectionController.php
+++ app/Http/Controllers/Admin/ElectionController.php
@@ -4,14 +4,9 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Admin\StoreElectionRequest;
-use App\Http\Requests\Admin\UpdateElectionRequest;
 use App\Models\Candidate;
-use App\Models\Department;
 use App\Models\Election;
-use App\Models\ElectionType;
-use App\Models\Position;
 use App\Models\User;
-use Illuminate\Database\Eloquent\Builder;
 use function view;
 
 class ElectionController extends Controller
@@ -32,12 +27,12 @@ class ElectionController extends Controller
 
     public function show(Election $election)
     {
-         $candidates = Candidate::ofElection($election)
-             ->with(['user', 'user.department', 'position'])
-             ->orderBy('position_id')
-             ->orderBy(User::select('name')
-                 ->whereColumn('candidates.user_id', 'users.id'))
-             ->get();
+        $candidates = Candidate::ofElection($election)
+            ->with(['user', 'user.department', 'position'])
+            ->orderBy('position_id')
+            ->orderBy(User::select('name')
+                ->whereColumn('candidates.user_id', 'users.id'))
+            ->get();
 
         return view('admin.elections.show', compact('election', 'candidates'));
     }

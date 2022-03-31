4a6b4ec43cd654b15e679759d134cd16380a4f3c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-03-31 21:19:44 +0800

    Call generateFileName method

--- app/Http/Controllers/Admin/ExportElectionResultController.php
+++ app/Http/Controllers/Admin/ExportElectionResultController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Exports\ExcelElectionResultExport;
 use App\Http\Controllers\Controller;
 use App\Models\Election;
+use App\Services\ElectionService;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
@@ -12,6 +13,8 @@ class ExportElectionResultController extends Controller
 {
     public function store(Election $election)
     {
-        return (new ExcelElectionResultExport($election))->download(str($election->title.'_'.now()->toDateString().'.xlsx')->snake());
+        $fileName = (new ElectionService($election))->generateFileName();
+        return (new ExcelElectionResultExport($election))
+            ->download($fileName);
     }
 }

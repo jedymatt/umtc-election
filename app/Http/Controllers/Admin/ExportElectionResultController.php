3073daddb710c307409e52da584189c7e353155d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-01 12:46:16 +0800

    Added abort unless election has ended  condition

--- app/Http/Controllers/Admin/ExportElectionResultController.php
+++ app/Http/Controllers/Admin/ExportElectionResultController.php
@@ -7,12 +7,15 @@ use App\Http\Controllers\Controller;
 use App\Models\Election;
 use App\Services\ElectionService;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ExportElectionResultController extends Controller
 {
     public function store(Election $election)
     {
+        abort_unless($election->hasEnded(), 403);
+
         $fileName = (new ElectionService($election))->generateFileName();
         return (new ExcelElectionResultExport($election))
             ->download($fileName);

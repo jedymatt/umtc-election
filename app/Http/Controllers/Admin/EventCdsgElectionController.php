48cf387b66230fa9980437a176757c1807aeee9c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-03 20:27:45 +0800

    Update EventCdsgElectionController.php

--- app/Http/Controllers/Admin/EventCdsgElectionController.php
+++ app/Http/Controllers/Admin/EventCdsgElectionController.php
@@ -3,9 +3,19 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
 use Illuminate\Http\Request;
 
 class EventCdsgElectionController extends Controller
 {
-    //
+    public function create(Event $event)
+    {
+        return view('admin.events.cdsg-elections.create', compact('event'));
+    }
+
+    public function store(Request $request, Event $event)
+    {
+
+        return redirect()->route('admin.events.show', $event);
+    }
 }

c11d6d02c88877ede4e42859328ed7473746c34d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-03 13:33:05 +0800

    Create Event.php

---
+++ app/Models/Event.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Event extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+    ];
+
+    public function elections(): HasMany
+    {
+        return $this->hasMany(Election::class);
+    }
+}

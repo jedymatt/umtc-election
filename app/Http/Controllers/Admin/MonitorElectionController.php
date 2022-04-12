4d75ef4f7ec010b7aba9eaf9010b6c5b8a8cad18
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-12 23:37:39 +0800

    Create MonitorElectionController.php

---
+++ app/Http/Controllers/Admin/MonitorElectionController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Election;
+use App\Models\Position;
+use App\Models\User;
+use App\Services\ElectionService;
+use Illuminate\Http\Request;
+
+class MonitorElectionController extends Controller
+{
+    public function show(Election $election)
+    {
+        $positions = Position::ofElectionType($election->electionType)->get();
+        $candidates = $election->candidates()
+            ->with(['position', 'user'])
+            ->withCount('votes')
+            ->orderBy(User::select('name')
+                ->whereColumn('candidates.user_id', 'users.id'))
+            ->get();
+
+        $calculatedWinners = $election->isEnded() ? (new ElectionService($election))->getCandidateWinners()
+            : collect();
+
+        return view('admin.monitor-elections', compact(
+            'election', 'positions', 'candidates', 'calculatedWinners'
+        ));
+    }
+}

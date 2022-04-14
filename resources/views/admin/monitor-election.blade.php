5f08b260f31c311663ccccb528cab84cf4e7ea0e
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-15 01:39:01 +0800

    Update monitor-election.blade.php

--- resources/views/admin/monitor-election.blade.php
+++ resources/views/admin/monitor-election.blade.php
@@ -19,12 +19,44 @@
 
                    @if($election->isEnded())
                         <div class="mt-4">
+                            <!-- component -->
+                            <div class="bg-white">
+
+                                <div class="overflow-x-auto border-x border-t">
+                                    <table class="table-auto w-full">
+                                        <thead class="border-b">
+                                        <tr class="bg-gray-100">
+                                            <th class="text-left p-4 font-medium">
+                                                Name
+                                            </th>
+                                            <th class="text-left p-4 font-medium">
+                                                Email
+                                            </th>
+                                            <th class="text-left p-4 font-medium">
+                                                Role
+                                            </th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <tr class="border-b hover:bg-gray-50">
+                                            <td class="p-4">
+                                                Prof. Lucie Waters
+                                            </td>
+                                            <td class="p-4">
+                                                basic@example.com
+                                            </td>
+                                            <td class="p-4">
+                                                Administrator
+                                            </td>
+                                        </tr>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
                             @foreach($calculatedWinners as $winners)
-                                @foreach($winners as $winner)
-                                    <div>
-                                        {{ $winner->user->name    }}
-                                    </div>
-                                @endforeach
+                               <div>
+                                   {{ $winners }}
+                               </div>
                             @endforeach
                         </div>
                    @endif

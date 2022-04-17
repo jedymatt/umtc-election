217e667fe1f73ff0a7675ed4c4b6e1da069dd253
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-17 14:40:46 +0800

    Add winners method

--- app/Services/ElectionService.php
+++ app/Services/ElectionService.php
@@ -8,6 +8,7 @@ use App\Models\ElectionType;
 use App\Models\Position;
 use App\Models\User;
 use App\Models\Vote;
+use App\Models\Winner;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Collection;
 
@@ -56,9 +57,9 @@ class ElectionService
      */
     public function getWinningCandidates(): Collection
     {
-        $candidates = Candidate::ofElection($this->election)->withCount('votes')
-            ->orderBy('position_id')
-            ->get();
+        $this->election->loadMissing('candidates')->loadCount('votes');
+
+        $candidates = $this->election->candidates->orderBy('position_id')->get();
 
         $candidates = $candidates->groupBy('position_id');
 
@@ -74,7 +75,51 @@ class ElectionService
         return $winningCandidates;
     }
 
+    public function saveWinners(): void
+    {
+        $winners = $this->getWinningCandidates()->map(function ($candidate) {
+            return Winner::make([
+                'candidate_id' => $candidate->id,
+                'election_id' => $this->election->id,
+                'votes' => $candidate->votes_count,
+            ]);
+        });
+
+        $this->election->winners()->saveMany($winners);
+    }
+
     public function hasWinnersConflict(): bool
+    {
+        $positionWinners = $this->election->winners()->with(['candidate'])->get()
+            ->groupBy('candidate.position_id');
+        foreach ($positionWinners as $winners) {
+            if ($winners->count() > 1) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public function getWinnersConflicts(): Collection
+    {
+        $this->election->winners->loadMissing(['candidate', 'candidate.position']);
+
+        $positionWinners = $this->election->winners()->get()->groupBy('candidate.position.name');
+
+        $winnersConflicts = collect();
+
+        foreach ($positionWinners as $positionName => $winners) {
+            if ($winners->count() > 1) {
+                $winnersConflicts[$positionName] = $winners;
+            }
+        }
+
+        return $winnersConflicts;
+    }
+
+
+    public function hasWinningCandidatesConflict(): bool
     {
         $candidates = Candidate::ofElection($this->election)
             ->withCount('votes')

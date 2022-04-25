5f44bf892e70a45958b70c8a54e48e3cd8138f14
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-25 13:59:04 +0800

    Remove the use of asset function

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,10 +11,10 @@
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
 
     <!-- Styles -->
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <link rel="stylesheet" href="/css/app.css">
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="/css/app.css" defer></script>
 
     @livewireStyles
 </head>

ca531b77770ef785c50527bfd73e146645df7560
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-27 12:25:59 +0800

    Swap window.Swal

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,22 +6,6 @@ window.Alpine = Alpine;
 
 Alpine.start();
 
-import Swal from 'sweetalert2';
+// import Swal from 'sweetalert2';
 
-window.Swal = Swal;
-
-window.toast = function ({icon = 'success', message = ''}) {
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 3000,
-        timerProgressBar: true,
-        showCloseButton: true,
-    })
-
-    Toast.fire({
-        icon: icon,
-        title: message,
-    })
-};
+// window.Swal = Swal;

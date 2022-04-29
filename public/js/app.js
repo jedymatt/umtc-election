ac7cc7652e48db7425755743393888afb6e03de2
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-04-29 19:54:13 +0800

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -5082,10 +5082,6 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
 alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
 
-window.testFunction = function () {
-  console.log('hello world');
-};
-
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":

1c57c6a247cf4766488c82bd8e4e7aca00afe280
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-01 18:04:00 +0800

    Update index.blade.php

--- resources/views/admin/admin-management/index.blade.php
+++ resources/views/admin/admin-management/index.blade.php
@@ -1,7 +1,7 @@
 <x-admin-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Admins') }}
+            {{ __('Admin Management') }}
         </h2>
     </x-slot>
 
@@ -11,7 +11,7 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     @can('create', \App\Models\Admin::class)
                         <div class="flex justify-end pb-4">
-                            <a href="{{ route('admin.admins.create') }}"
+                            <a href="{{ route('admin.admin-management.create') }}"
                                class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150">
                                 Create Admin Account</a>
                         </div>

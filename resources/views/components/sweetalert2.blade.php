48f68e2ec564087ccf99eddbe35edb35205e1c02
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-02 11:55:19 +0800

    Moved code to sweetalert2.js then call function

--- resources/views/components/sweetalert2.blade.php
+++ resources/views/components/sweetalert2.blade.php
@@ -1,19 +1,10 @@
 <!-- SweetAlert2 script -->
-@if(session()->has('success'))
+@if (session()->has('success'))
     <script src="{{ asset('js/sweetalert2.js') }}"></script>
     <script>
-        const Toast = Swal.mixin({
-            toast: true,
-            position: 'top-end',
-            showConfirmButton: false,
-            timer: 3000,
-            timerProgressBar: true,
-            showCloseButton: true,
-        });
-
-        Toast.fire({
+        fireToast({
             icon: 'success',
-            title: {{ Js::from(session('success'))}},
+            message: {{ Js::from(session('success')) }},
         });
     </script>
 @endif

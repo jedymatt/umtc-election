359594850f578bd3f064023a0e168b347cb57c31
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-02 11:54:24 +0800

    Update sweetalert2.js

--- resources/js/sweetalert2.js
+++ resources/js/sweetalert2.js
@@ -1,3 +1,20 @@
 import Swal from "sweetalert2";
 
 window.Swal = Swal;
+
+window.fireToast = ({ icon, message }) => {
+    const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 5000,
+        timerProgressBar: true,
+        showCloseButton: true,
+        padding: '1rem',
+    });
+
+    Toast.fire({
+        icon: icon,
+        title: message,
+    });
+};

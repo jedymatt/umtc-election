75fe3619a9414c1ed13787045cb3e4cbe3dda467
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-04 01:43:45 +0800

    make file content to a variable

--- setup.sh
+++ setup.sh
@@ -10,7 +10,7 @@ php artisan optimize
 
 sudo apt-get install supervisor
 
-sudo bash -c 'cat << EOF >/etc/supervisor/conf.d/laravel-worker.conf
+worker_file_content='
 [program:laravel-worker]
 process_name=%(program_name)s_%(process_num)02d
 command=php /var/www/laravel/artisan queue:work --sleep=3 --tries=3 --max-time=3600
@@ -23,6 +23,10 @@ numprocs=1
 redirect_stderr=true
 stdout_logfile=/var/www/laravel/storage/logs/worker.log
 stopwaitsecs=3600
+'
+
+sudo bash -c 'cat << EOF >/etc/supervisor/conf.d/laravel-worker.conf
+$worker_file_content
 EOF'
 
 sudo supervisorctl reread

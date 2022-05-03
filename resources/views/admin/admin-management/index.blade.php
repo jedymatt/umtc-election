b1edf1a3acd3e2efdb354dca1f9bbcc5272d26a9
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-03 18:10:33 +0800

    Change color green to custom green

--- resources/views/admin/admin-management/index.blade.php
+++ resources/views/admin/admin-management/index.blade.php
@@ -42,13 +42,9 @@
                                                     {{ $admin->name }}
                                                 </span>
                                             @if (auth()->id() == $admin->id)
-                                                <svg xmlns="http://www.w3.org/2000/svg"
-                                                     class="h-6 w-6 inline-block text-green-500"
-                                                     fill="none" viewBox="0 0 24 24" stroke="currentColor"
-                                                     stroke-width="2">
-                                                    <path stroke-linecap="round" stroke-linejoin="round"
-                                                          d="M5.636 18.364a9 9 0 010-12.728m12.728 0a9 9 0 010 12.728m-9.9-2.829a5 5 0 010-7.07m7.072 0a5 5 0 010 7.07M13 12a1 1 0 11-2 0 1 1 0 012 0z"/>
-                                                </svg>
+                                                <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5 inline-block text-[#05b714]" viewBox="0 0 20 20" fill="currentColor">
+                                                    <path fill-rule="evenodd" d="M5.05 3.636a1 1 0 010 1.414 7 7 0 000 9.9 1 1 0 11-1.414 1.414 9 9 0 010-12.728 1 1 0 011.414 0zm9.9 0a1 1 0 011.414 0 9 9 0 010 12.728 1 1 0 11-1.414-1.414 7 7 0 000-9.9 1 1 0 010-1.414zM7.879 6.464a1 1 0 010 1.414 3 3 0 000 4.243 1 1 0 11-1.415 1.414 5 5 0 010-7.07 1 1 0 011.415 0zm4.242 0a1 1 0 011.415 0 5 5 0 010 7.072 1 1 0 01-1.415-1.415 3 3 0 000-4.242 1 1 0 010-1.415zM10 9a1 1 0 011 1v.01a1 1 0 11-2 0V10a1 1 0 011-1z" clip-rule="evenodd" />
+                                                  </svg>
                                             @endif
                                             <span
                                                 class="block font-light text-gray-500 text-sm">{{ $admin->email }}</span>

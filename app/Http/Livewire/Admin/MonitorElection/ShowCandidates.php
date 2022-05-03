f7b3c218878d0feb97870044f79da216cce2ec49
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-03 16:00:39 +0800

    Optimize code

--- app/Http/Livewire/Admin/MonitorElection/ShowCandidates.php
+++ app/Http/Livewire/Admin/MonitorElection/ShowCandidates.php
@@ -9,24 +9,35 @@ use Livewire\Component;
 class ShowCandidates extends Component
 {
     public $election;
+    public $positions;
+    public $candidates;
 
     public function mount(Election $election)
     {
+        $this->election->loadMissing('electionType', 'electionType.positions');
         $this->election = $election;
+        $this->positions = $election->electionType->positions;
+        $this->candidates = $this->queryCandidates();
     }
 
     public function render()
     {
-        $this->election->loadMissing('electionType', 'electionType.positions');
-        $positions = $this->election->electionType->positions;
-        $candidates = $this->election->candidates()
-            ->with(['position', 'user', 'user.department'])
+        return view('livewire.admin.monitor-election.show-candidates');
+    }
+
+    public function refreshCandidates()
+    {
+        $this->candidates = $this->queryCandidates();
+    }
+
+    public function queryCandidates()
+    {
+        return $this->election->candidates()
+            ->select('position_id', 'user_id')
+            ->with(['user:id,department_id,name', 'user.department:id,name'])
             ->withCount('votes')
             ->orderBy(User::select('name')
                 ->whereColumn('candidates.user_id', 'users.id'))
             ->get();
-        return view('livewire.admin.monitor-election.show-candidates',
-            compact('positions', 'candidates'),
-        );
     }
 }

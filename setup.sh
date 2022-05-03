347eebab9d101110301f53cfb85116a30735b1e1
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-04 00:54:02 +0800

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+sudo apt install php8.1-xml  php8.1-gd php8.1-zip php8.1-mysql
+
+
+cd /var/www/laravel && composer install --optimize-autoloader --no-dev
+
+php artisan key:generate
+
+php artisan optimize
+
+sudo apt-get install supervisor
+
+sudo bash -c 'cat << EOF >> /etc/supervisor/conf.d/laravel-worker.conf
+[program:laravel-worker]
+process_name=%(program_name)s_%(process_num)02d
+command=php /var/www/laravel/artisan queue:work --sleep=3 --tries=3 --max-time=3600
+autostart=true
+autorestart=true
+stopasgroup=true
+killasgroup=true
+user=forge
+numprocs=1
+redirect_stderr=true
+stdout_logfile=/var/www/laravel/storage/logs/worker.log
+stopwaitsecs=3600
+EOF'
+
+echo "Run 'crontab -e' and append this line:"
+echo "* * * * * cd /path-to-your-project && php artisan schedule:run >> /dev/null 2>&1"

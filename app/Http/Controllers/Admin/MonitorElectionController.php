086ce6436d4c6ea07696099e948857a229d84315
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-03 16:00:56 +0800

    Removed unused variables

--- app/Http/Controllers/Admin/MonitorElectionController.php
+++ app/Http/Controllers/Admin/MonitorElectionController.php
@@ -15,20 +15,13 @@ class MonitorElectionController extends Controller
     public function show(Election $election)
     {
         $election->loadMissing('electionType', 'electionType.positions');
-        $positions = $election->electionType->positions;
-        $candidates = $election->candidates()
-            ->with(['position', 'user', 'user.department'])
-            ->withCount('votes')
-            ->orderBy(User::select('name')
-                ->whereColumn('candidates.user_id', 'users.id'))
-            ->get();
 
         if ($election->isEnded() && $election->winners()->doesntExist()) {
             (new ElectionService($election))->saveWinners();
         }
 
         return view('admin.monitor-election', compact(
-            'election', 'positions', 'candidates',
+            'election',
         ));
     }
 }

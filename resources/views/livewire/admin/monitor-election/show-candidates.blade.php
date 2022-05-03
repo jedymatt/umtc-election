468fdc162973371fdd2354390730d6325ceac78d
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-03 12:32:14 +0800

    Create show-candidates.blade.php

---
+++ resources/views/livewire/admin/monitor-election/show-candidates.blade.php
@@ -0,0 +1,48 @@
+<div wire:poll>
+    <div class="overflow-x-auto border-x border-t">
+        <table class="table-auto w-full">
+            <thead class="border-b-2 border-b-gray-300">
+            <tr class="bg-gray-100">
+                <th class="text-left p-4 font-medium">
+                    Name
+                </th>
+                <th class="text-left p-4 font-medium">
+                    Department
+                </th>
+                <th class="text-left p-4 font-medium">
+                    Votes
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            @foreach ($positions as $position)
+                <tr class="border-b border-l-8 border-l-primary ">
+                    <td class="p-4" colspan="3">
+                        <span class="font-medium text-primary">{{ $position->name }}</span>
+                    </td>
+                </tr>
+                @forelse ($candidates->where('position_id', $position->id) as $candidate)
+                    <tr class="border-b hover:bg-gray-50">
+                        <td class="p-4">
+                            {{ $candidate->user->name }}
+                        </td>
+                        <td class="p-4">
+                            {{ $candidate->user->department->name }}
+                        </td>
+                        <td class="p-4">
+                            {{ $candidate->votes_count }}
+                        </td>
+                    </tr>
+
+                @empty
+                    <tr class="border-b bg-gray-50">
+                        <td class="p-4" colspan="3">
+                            <span class="text-gray-400 italic">No Candidates</span>
+                        </td>
+                    </tr>
+                @endforelse
+            @endforeach
+            </tbody>
+        </table>
+    </div>
+</div>

c9f7474e1cc16db454ee481c16ff00188e6ea2c8
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-03 16:04:07 +0800

    Moved comment inside the @if

--- resources/views/components/sweetalert2.blade.php
+++ resources/views/components/sweetalert2.blade.php
@@ -1,5 +1,5 @@
-<!-- SweetAlert2 script -->
 @if (session()->has('success'))
+    <!-- SweetAlert2 script -->
     <script src="{{ asset('js/sweetalert2.js') }}"></script>
     <script>
         fireToast({

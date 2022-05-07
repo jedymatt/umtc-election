c6e11f80f4e053ec2d4b1e51524ade6125e60d9e
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-07 18:30:11 +0800

    Remove unused imports and remove comment `new Channel`

--- app/Events/VoteSubmitted.php
+++ app/Events/VoteSubmitted.php
@@ -3,12 +3,9 @@
 namespace App\Events;
 
 use App\Models\Election;
-use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Broadcasting\PresenceChannel;
 use Illuminate\Broadcasting\PrivateChannel;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
@@ -35,7 +32,6 @@ class VoteSubmitted implements ShouldBroadcast
      */
     public function broadcastOn()
     {
-//        return new Channel('election.'. $this->electionId);
         return new PrivateChannel('election.' . $this->electionId);
     }
 }

263c064e67c6af59dccd21d0a9a0f1b4addbe1b2
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-07 18:27:17 +0800

    Revert to ShouldBroadcast

--- app/Events/VoteSubmitted.php
+++ app/Events/VoteSubmitted.php
@@ -12,7 +12,7 @@ use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class VoteSubmitted implements ShouldBroadcastNow
+class VoteSubmitted implements ShouldBroadcast
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 

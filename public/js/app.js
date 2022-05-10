c0388a43c2ae9b9090a7f395f11ba3c477c468cd
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-10 21:14:28 +0800

    Update packages and build assets

--- public/js/app.js
+++ public/js/app.js
@@ -5093,7 +5093,6 @@ alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
 "use strict";
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var laravel_echo__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! laravel-echo */ "./node_modules/laravel-echo/dist/echo.js");
-/* provided dependency */ var process = __webpack_require__(/*! process/browser.js */ "./node_modules/process/browser.js");
 window._ = __webpack_require__(/*! lodash */ "./node_modules/lodash/lodash.js");
 /**
  * We'll load the axios HTTP library which allows us to easily issue requests
@@ -5113,8 +5112,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 window.Pusher = __webpack_require__(/*! pusher-js */ "./node_modules/pusher-js/dist/web/pusher.js");
 window.Echo = new laravel_echo__WEBPACK_IMPORTED_MODULE_0__["default"]({
   broadcaster: 'pusher',
-  key: process.env.MIX_PUSHER_APP_KEY,
-  cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+  key: "977de0de47837a9f9a38",
+  cluster: "ap1",
   forceTLS: true
 });
 

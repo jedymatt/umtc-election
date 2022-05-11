383dbf68141eee1435b9355e4b7d568560592a92
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-11 23:49:54 +0800

    Cascade on delete user_id field

--- database/migrations/2022_03_06_205212_create_votes_table.php
+++ database/migrations/2022_03_06_205212_create_votes_table.php
@@ -14,7 +14,7 @@ return new class extends Migration {
     {
         Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('election_id')->constrained();
             $table->timestamps();
         });

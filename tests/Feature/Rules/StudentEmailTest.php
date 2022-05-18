0040acaac1441e7cdd936d1d844cd6e805eaf543
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-18 11:02:33 +0800

    Update StudentEmailTest.php

--- tests/Feature/Rules/StudentEmailTest.php
+++ tests/Feature/Rules/StudentEmailTest.php
@@ -3,140 +3,105 @@
 namespace Rules;
 
 use App\Rules\StudentEmail;
-use Illuminate\Support\Str;
 use PHPUnit\Framework\TestCase;
 
 class StudentEmailTest extends TestCase
 {
+    public function test_invalid_email_format()
+    {
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'f.lastname.123456.tc@'
+        ));
+
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            '@umindanao.edu.ph'
+        ));
+
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'f.lastname.123456.tc_umindanano.edu.ph',
+        ));
+    }
+
     public function test_no_initial()
     {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'lastname.123456.tc@umindanao.edu.ph'
-                )
-        );
-    }
-
-    public function test_no_domain()
-    {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'f.lastname.123456.tc@'
-                )
-        );
-    }
-
-    public function test_no_username()
-    {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    '@umindanao.edu.ph'
-                )
-        );
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'lastname.123456.tc@umindanao.edu.ph'
+        ));
     }
 
     public function test_initial_extra_character()
     {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'fextracharacter.lastname.123456.tc@umindanao.edu.ph'
-                )
-        );
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'fextracharacter.lastname.123456.tc@umindanao.edu.ph'
+        ));
     }
 
     public function test_not_institutional_domain()
     {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'fs.lastname.123456.tc@gmail.com'
-                )
-        );
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'f.lastname.123456.tc@gmail.com'
+        ));
     }
 
-    public function test_not_email()
+    public function test_empty_postfix_in_username()
     {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    Str::random(),
-                )
-        );
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'f.lastname.123456.@umindanao.edu.ph'
+        ));
     }
 
-    public function test_no_tc_postfix_in_username()
+    public function test_invalid_postfix()
     {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'f.lastname.123456@umindanao.edu.ph'
-                )
-        );
-    }
-
-    public function test_postfix_not_tc()
-    {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'f.lastname.123456.nottc@umindanao.edu.ph'
-                )
-        );
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'f.lastname.123456.invalid@umindanao.edu.ph'
+        ));
     }
 
     public function test_student_id_contains_letter()
     {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'f.lastname.12A456.tc@umindanao.edu.ph'
-                )
-        );
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'f.lastname.12A456.tc@umindanao.edu.ph'
+        ));
     }
 
     public function test_student_id_extra_length()
     {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'f.lastname.1234567.tc@umindanao.edu.ph'
-                )
-        );
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'f.lastname.1234567.tc@umindanao.edu.ph'
+        ));
     }
 
     public function test_student_id_lesser_length()
     {
-        $this->assertFalse(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'f.lastname.12345.tc@umindanao.edu.ph'
-                )
-        );
+        $this->assertFalse((new StudentEmail)->passes(
+            'email',
+            'f.lastname.12345.tc@umindanao.edu.ph'
+        ));
     }
 
-    public function test_valid_email()
+    public function test_valid_um_main_email()
     {
-        $this->assertTrue(
-            (new StudentEmail)
-                ->passes(
-                    'email',
-                    'f.lastname.123456.tc@umindanao.edu.ph'
-                )
-        );
+        $this->assertTrue((new StudentEmail)->passes(
+            'email',
+            'f.lastname.123456@umindanao.edu.ph'
+        ));
+    }
+
+    public function test_valid_um_tagum_email()
+    {
+        $this->assertTrue((new StudentEmail)->passes(
+            'email',
+            'f.lastname.123456.tc@umindanao.edu.ph'
+        ));
     }
 }

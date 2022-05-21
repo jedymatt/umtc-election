7e3629b03d6962eaa3d547eac12fd0c4224a78f1
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-21 21:01:57 +0800

    Update result.blade.php

--- resources/views/admin/elections/result.blade.php
+++ resources/views/admin/elections/result.blade.php
@@ -1,4 +1,4 @@
-<x-app-layout>
+<x-admin-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
             {{ __('Election Result') }}
@@ -51,5 +51,5 @@
             </div>
         </div>
     </div>
-</x-app-layout>
+</x-admin-app-layout>
 

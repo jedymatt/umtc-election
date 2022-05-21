beca4721869ca1d5dcfb1109351844a17c439993
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-21 20:38:39 +0800

    Remove unused import

--- app/Http/Controllers/Admin/ElectionController.php
+++ app/Http/Controllers/Admin/ElectionController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\Admin\StoreElectionRequest;
 use App\Models\Candidate;
 use App\Models\Election;
 use App\Models\User;

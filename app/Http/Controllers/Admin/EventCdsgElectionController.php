e0b82778b1f96f411e116a43281522e2820792e6
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-05-28 18:08:11 +0800

    Cleanup controller code

--- app/Http/Controllers/Admin/EventCdsgElectionController.php
+++ app/Http/Controllers/Admin/EventCdsgElectionController.php
@@ -2,18 +2,26 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
 use App\Models\Admin;
+use App\Models\Event;
 use App\Models\Election;
 use App\Models\ElectionType;
-use App\Models\Event;
-use App\Services\ElectionService;
-use App\Services\EventService;
 use Illuminate\Http\Request;
+use App\Services\EventService;
+use App\Services\ElectionService;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\StoreCdsgElectionRequest;
 use Illuminate\Support\Facades\Validator;
 
 class EventCdsgElectionController extends Controller
 {
+    protected $electionService;
+
+    public function __construct(ElectionService $electionService)
+    {
+        $this->electionService = $electionService;
+    }
+
     public function create(Event $event)
     {
         /** @var Admin $admin */
@@ -24,38 +32,19 @@ class EventCdsgElectionController extends Controller
         return view('admin.events.cdsg-elections.create', compact('event'));
     }
 
-    public function store(Request $request, Event $event)
+    public function store(StoreCdsgElectionRequest $request, Event $event)
     {
-        /** @var Admin $admin */
-        $admin = auth('admin')->user();
+        $validated = $request->validated();
 
-        abort_if(!empty(EventService::createCdsgElectionFailureMessage($event, $admin)), 403, 'Cannot create election');
-
-        $dsgElections = $event->dsgElections;
-
-        foreach ($dsgElections as $election) {
-            abort_if($election->hasConflictedWinners(), 403, 'Detected Election Winners Conflict');
-        }
-
-        $validator = Validator::make($request->all(), [
-            'title' => 'required|string',
-            'description' => 'nullable|string',
-            'start_at' => 'required|date|before:end_at',
-            'end_at' => 'required|date|after:start_at',
-            'candidates.*.user_id' => 'integer',
-            'candidates.*.position_id' => 'integer',
+        $election = $this->electionService->createCdsgElection([
+            'title' => $validated['title'],
+            'description' => $validated['description'],
+            'start_at' => $validated['start_at'],
+            'end_at' => $validated['end_at'],
+            'event_id' => $event->id,
         ]);
 
-        $validated = $validator->validate();
-
-        $election = Election::make($validated);
-        $election->election_type_id = ElectionType::TYPE_CDSG;
-        $election->event_id = $event->id;
-        $election->save();
-
-        if (array_key_exists('candidates', $validated)) {
-            $election->candidates()->createMany($validated['candidates']);
-        }
+        $election->candidates()->createMany($validated['candidates'] ?? []);
 
         return redirect()->route('admin.events.show', $event)
             ->with('success', 'CDSG election successfully created!');

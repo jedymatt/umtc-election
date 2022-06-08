28299887bc7f7ab4d3b36898e2478dcf485b30b9
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-08 13:51:32 +0000

    Removed browser-sync

--- webpack.mix.js
+++ webpack.mix.js
@@ -16,8 +16,4 @@ mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'pub
     require('autoprefixer'),
 ]);
 
-mix.browserSync({
-    proxy: 'http://localhost:8000',
-})
-
 mix.disableNotifications();

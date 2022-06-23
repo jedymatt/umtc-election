2ca5d3468673098d08d7c2eaf19429bdc2d75726
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-06-23 10:35:00 +0800

    Format add-candidates-form

--- resources/views/livewire/admin/cdsg-election/add-candidates-form.blade.php
+++ resources/views/livewire/admin/cdsg-election/add-candidates-form.blade.php
@@ -2,10 +2,9 @@
     <div>
         <div class="sm:flex gap-4">
             <input wire:model.debounce.500ms="searchText" type="search" placeholder="Search using name or email"
-                   class="rounded-md w-full inline-block"
-            >
+                class="rounded-md w-full inline-block">
             <select wire:model="selectedPositionId" class="rounded-md inline-block w-full mt-1 sm:mt-0">
-                @foreach($positions as $position)
+                @foreach ($positions as $position)
                     <option value="{{ $position->id }}">
                         {{ $position->name }}
                     </option>
@@ -14,11 +13,10 @@
         </div>
         <div class="mt-1">
             <ul>
-                @foreach($users as $user)
+                @foreach ($users as $user)
                     <li class="border-b">
                         <a href="#" wire:click.prevent="addCandidate({{ $user->id }})"
-                           class="block hover:bg-gray-100 w-full px-4 py-2"
-                        >
+                            class="block hover:bg-gray-100 w-full px-4 py-2">
                             {{ $user->name }}
                             <span class="block text-sm text-gray-800">
                                 {{ $user->email }}
@@ -29,8 +27,7 @@
             </ul>
             <div class="text-gray-600 text-sm px-4 border-b py-2">
 
-                @if($users->isNotEmpty())
-
+                @if ($users->isNotEmpty())
                     Showing {{ $users->firstItem() }} - {{ $users->lastItem() }} of {{ $users->total() }} users
                 @elseif($searchText != '')
                     No results
@@ -45,50 +42,49 @@
                     <div class="overflow-hidden">
                         <table class="min-w-full">
                             <thead class="border-b">
-                            <tr>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                    Name
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                    Position
-                                </th>
-                                <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
-                                    Action
-                                </th>
-                            </tr>
+                                <tr>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Name
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Position
+                                    </th>
+                                    <th scope="col" class="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                                        Action
+                                    </th>
+                                </tr>
                             </thead>
                             <tbody>
-                            @forelse($candidates as $candidate)
-                                <tr class="border-b">
-                                    <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                                        {{ $candidate['user_name'] }}
-                                        <div class="font-light text-gray-700">
-                                            {{ $candidate['user_email'] }}
-                                        </div>
-                                    </td>
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        {{ $candidate['position_name'] }}
-                                    </td>
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
-                                        <button class="text-red-500 hover:underline" type="button"
-                                                wire:click.prevent="removeCandidate({{ json_encode($candidate)  }})">
-                                            Remove
-                                        </button>
-                                    </td>
-                                    <input type="hidden" name="candidates[{{ $loop->index }}][user_id]"
-                                           value="{{ $candidate['user_id']  }}">
-                                    <input type="hidden" name="candidates[{{ $loop->index }}][position_id]"
-                                           value="{{ $candidate['position_id'] }}">
-                                </tr>
-                            @empty
-                                <tr class="border-b">
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
-                                        colspan="3"
-                                    >
-                                        <span class="flex justify-center">Empty Candidates</span>
-                                    </td>
-                                </tr>
-                            @endforelse
+                                @forelse($candidates as $candidate)
+                                    <tr class="border-b">
+                                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                            {{ $candidate['user_name'] }}
+                                            <div class="font-light text-gray-700">
+                                                {{ $candidate['user_email'] }}
+                                            </div>
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            {{ $candidate['position_name'] }}
+                                        </td>
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                            <button class="text-red-500 hover:underline" type="button"
+                                                wire:click.prevent="removeCandidate({{ json_encode($candidate) }})">
+                                                Remove
+                                            </button>
+                                        </td>
+                                        <input type="hidden" name="candidates[{{ $loop->index }}][user_id]"
+                                            value="{{ $candidate['user_id'] }}">
+                                        <input type="hidden" name="candidates[{{ $loop->index }}][position_id]"
+                                            value="{{ $candidate['position_id'] }}">
+                                    </tr>
+                                @empty
+                                    <tr class="border-b">
+                                        <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap"
+                                            colspan="3">
+                                            <span class="flex justify-center">Empty Candidates</span>
+                                        </td>
+                                    </tr>
+                                @endforelse
                             </tbody>
                         </table>
                     </div>

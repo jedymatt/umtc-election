64d0815ddda3960efd380de57ccfe94573c623b5
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-21 10:29:37 +0000

    Format

--- resources/views/components/sweetalert2.blade.php
+++ resources/views/components/sweetalert2.blade.php
@@ -1,31 +1,32 @@
 @if (session()->has('success') || session()->has('error') || session()->has('warning'))
     <!-- Toast -->
     <script>
-        window.addEventListener('DOMContentLoaded', function () {
+        window.addEventListener('DOMContentLoaded', function() {
+            console.log('fireToast');
             @if (session()->has('success'))
-            fireToast({
-                icon: 'success',
-                message: @js(session('success')),
-            });
+                fireToast({
+                    icon: 'success',
+                    message: @js(session('success')),
+                });
             @endif
             @if (session()->has('success'))
-            fireToast({
-                icon: 'success',
-                message: @js(session('success')),
-            });
+                fireToast({
+                    icon: 'success',
+                    message: @js(session('success')),
+                });
             @endif
             @if (session()->has('error'))
-            fireToast({
-                icon: 'error',
-                message: @js(session('error')),
-            });
+                fireToast({
+                    icon: 'error',
+                    message: @js(session('error')),
+                });
             @endif
 
             @if (session()->has('warning'))
-            fireToast({
-                icon: 'warning',
-                message: @js(session('warning')),
-            });
+                fireToast({
+                    icon: 'warning',
+                    message: @js(session('warning')),
+                });
             @endif
         });
     </script>

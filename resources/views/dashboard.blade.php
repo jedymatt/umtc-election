766b3e84d6df8a8f747f0418c79e3a401c727559
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-23 12:56:39 +0000

    Update dashboard.blade.php

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,7 +9,24 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    You're logged in!
+                    <div class="flex justify-between">
+                        <h1 class="text-2xl text-primary font-bold">Available elections</h1>
+                        <a href="{{ route('elections.index') }}"
+                            class="text-primary text-sm inline-flex justify-center items-center hover:bg-primary-100 rounded-md p-1">
+                            <span class="pl-1.5">More Elections</span>
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20"
+                                fill="currentColor">
+                                <path fill-rule="evenodd"
+                                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                                    clip-rule="evenodd" />
+                            </svg>
+                        </a>
+                    </div>
+                    <div class="mt-4">
+                        @foreach ($elections as $election)
+                            <x-election-card :election="$election" />
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>

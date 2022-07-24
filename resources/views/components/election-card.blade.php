1724a26de69d488620026142b1f5026b61f61c87
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-24 07:04:06 +0000

    Update election-card component

--- resources/views/components/election-card.blade.php
+++ resources/views/components/election-card.blade.php
@@ -1,12 +1,26 @@
-@props(['election'])
+@props(['election', 'disableVote' => false])
 
-<div class="rounded-md bg-white border-2 border-primary-400 p-4 max-w-lg">
-    <h1 class="text-2xl font-bold text-primary-500">{{ $election->title }}</h1>
-    <div class="mt-4">
-        Deadline: {{ $election->end_at->toDayDateTimeString() }}
+<div class="rounded-md shadow-sm focus:ring border border-gray-200 p-6 flex flex-col">
+    <div class="flex-grow">
+        <div class="font-bold text-2xl text-primary">
+            {{ $election->title }}
+        </div>
+        <div class="text-md mt-4">
+            Deadline: {{ $election->end_at->toDayDateTimeString() }}
+        </div>
+        <div class="text-gray-600 mt-1">
+            <span class="inline-block text-sm border rounded-full p-1 bg-gray-100">
+                {{ $election->electionType->name }}
+            </span>
+            @if ($election->department != null)
+                <span class="inline-block text-sm border rounded-full p-1 my-1 bg-gray-100">
+                    {{ $election->department->name }}
+                </span>
+            @endif
+        </div>
     </div>
-    <div class="mt-4 flex justify-end">
-        <x-button-primary href="{{ route('elections.vote', $election) }}">
+    <div class="pt-4 flex justify-end">
+        <x-button-primary href="{{ route('elections.vote', $election) }}" :disabled="$disableVote">
             Vote
         </x-button-primary>
     </div>

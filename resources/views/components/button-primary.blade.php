5b1019922131b81ac3c569f53035b5a4d5e436d9
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-24 06:59:50 +0000

    Make tag to button when disabled

--- resources/views/components/button-primary.blade.php
+++ resources/views/components/button-primary.blade.php
@@ -1,8 +1,9 @@
 @php
 $tag = $attributes['href'] ? 'a' : 'button';
+$tag = $attributes['disabled'] ? 'button' : $tag;
 @endphp
 
 <{{ $tag }}
-    {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-primary-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-500 active:bg-primary-700 focus:outline-none focus:border-primary-700 focus:ring ring-primary-200 disabled:opacity-25 transition ease-in-out duration-150']) }}>
+    {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-primary-600 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-primary-500 active:bg-primary-700 focus:outline-none focus:border-primary-700 focus:ring ring-primary-200 disabled:opacity-25 transition ease-in-out duration-150 disabled:pointer-events-none']) }}>
     {{ $slot }}
 </{{ $tag }}>

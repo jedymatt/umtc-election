1afe6184e5ac7c7dff28c2295758b4634d051430
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-07-27 02:41:36 +0000

    Add comment

--- app/Rules/StudentEmail.php
+++ app/Rules/StudentEmail.php
@@ -15,6 +15,13 @@ class StudentEmail implements Rule
      */
     public function passes($attribute, $value)
     {
+        // ^ Start of string
+        // [a-z] Initial of the student's first name
+        // \.[a-z]* Dot followed Student's last name
+        // \.\d{6} Dot followed by Student's ID (6 digits)
+        // (\.tc)? Student's school if .tc is present then, it is in um tagum branch else it is um main branch
+        // @umindanao\.edu\.ph email domain
+        // $ End of string
         return preg_match('/^[a-z]\.[a-z]*\.\d{6}(\.tc)?@umindanao\.edu\.ph$/', $value) === 1;
     }
 

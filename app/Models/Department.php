e1d402d81d11ad5c41f642323fbb16bc7768e54b
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-16 04:32:29 +0000

    Add scope doesntHaveDsgElectionOfEvent

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Builder;
 
 class Department extends Model
 {
@@ -24,5 +25,15 @@ class Department extends Model
     public function getAcronymName(): string
     {
         return preg_replace('/\b([A-Z])|./', '$1', $this->name);
+    }
+
+    public function scopeDoesntHaveDsgElectionOfEvent(Builder $query, Event $event)
+    {
+        return $query->whereNotIn('id', function (Builder $query) use ($event) {
+            $query->select('department_id')
+                ->from('elections')
+                ->where('election_type_id', '=', ElectionType::TYPE_DSG)
+                ->where('event_id', '=', $event->id);
+        });
     }
 }

99ae811954eb6f8b0ba30aa66575ef157841fb0c
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-09-16 05:53:45 +0000

    Replace whereNotIn with whereNotExists in Department scopeDoesntHaveDsgElectionOfEvent

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Department extends Model
 {
@@ -27,13 +28,15 @@ class Department extends Model
         return preg_replace('/\b([A-Z])|./', '$1', $this->name);
     }
 
-    public function scopeDoesntHaveDsgElectionOfEvent(Builder $query, Event $event)
+    public function scopeDoesntHaveDsgElectionOfEvent(EloquentBuilder $query, Event $event)
     {
-        return $query->whereNotIn('id', function (Builder $query) use ($event) {
-            $query->select('department_id')
+        return $query->whereNotExists(function (QueryBuilder $query) use ($event) {
+            $query->select()
                 ->from('elections')
+                ->whereColumn('department_id', '=', 'departments.id')
                 ->where('election_type_id', '=', ElectionType::TYPE_DSG)
-                ->where('event_id', '=', $event->id);
+                ->where('event_id', '=', $event->id)
+                ->whereNotNull('department_id');
         });
     }
 }

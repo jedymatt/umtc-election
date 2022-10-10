e2aa956c2e580389b3643bfbad216b0c28117ac5
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-10-10 07:33:22 +0000

    Revert border color to default

--- resources/views/elections/index.blade.php
+++ resources/views/elections/index.blade.php
@@ -10,7 +10,7 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     @if ($userHasNoDepartment)
-                        <div class="mb-4 rounded-md shadow-md p-4 border border-gray-100">
+                        <div class="mb-4 rounded-md shadow-md p-4 border">
                             <div class="flex items-center">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-yellow-500" fill="none"
                                     viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">

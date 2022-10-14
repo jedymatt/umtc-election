12bf50e0d3125e6c2066b3c2a8102e2c086597e7
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-10-14 07:27:56 +0000

    Lint

--- app/Models/Department.php
+++ app/Models/Department.php
@@ -2,11 +2,9 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Department extends Model
 {

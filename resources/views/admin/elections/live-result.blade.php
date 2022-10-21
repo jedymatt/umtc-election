2afe5b7f50bc28e06814daa72b581ce6d108981f
Jedy Matt Tabasco <jedymatt@gmail.com>
2022-10-21 10:00:17 +0000

    Update named component

--- resources/views/admin/elections/live-result.blade.php
+++ resources/views/admin/elections/live-result.blade.php
@@ -9,9 +9,8 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <!-- content here -->
                     <div class="container">
-                        <livewire:admin.monitor-election.show-candidates :election="$election"/>
+                        <livewire:admin.election.show-live-result :election="$election"/>
                     </div>
                 </div>
             </div>

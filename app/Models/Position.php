390ffb6800b78c8ae0333f6395c414641243e2d1
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-03-17 21:04:32 +0800

    Remove unused relationship and local scopes in Position model

--- app/Models/Position.php
+++ app/Models/Position.php
@@ -13,26 +13,4 @@ class Position extends Model
 
     public $timestamps = false;
 
-    public function electionType(): BelongsToMany
-    {
-        return $this->belongsToMany(ElectionType::class);
-    }
-
-    public function scopeCdsgElection(Builder $query)
-    {
-        $query->whereRelation('electionType', 'id', ElectionType::TYPE_CDSG)
-            ->orderBy('id');
-    }
-
-    public function scopeDsgElection(Builder $query)
-    {
-        $query->whereRelation('electionType', 'id', ElectionType::TYPE_DSG)
-            ->orderBy('id');
-    }
-
-    public function scopeOfElectionType(Builder $query, ElectionType $electionType)
-    {
-        $query->whereRelation('electionType', 'id', $electionType->id)
-            ->orderBy('id');
-    }
 }

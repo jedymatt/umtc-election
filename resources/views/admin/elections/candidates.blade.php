b3a0a034c195d6de2227105f10c5ebd9f77145c5
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-03-21 21:28:48 +0800

    Show election tabs

--- resources/views/admin/elections/candidates.blade.php
+++ resources/views/admin/elections/candidates.blade.php
@@ -5,7 +5,12 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 space-y-6">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                @include('admin.elections.partials.tabs')
+            </div>
+        </div>
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="border-b border-gray-200 bg-white p-6">
@@ -25,7 +30,7 @@
                                     clip-rule="evenodd"
                                 />
                             </svg>
-                        </x-button-primary>
+                            </x-button-primary>
                     </div>
                     <div class="mt-4 h-full min-h-[50vh]">
                         @livewire('admin.election.manage-candidates', ['election' => $election])
--- resources/views/admin/elections/show.blade.php
+++ resources/views/admin/elections/show.blade.php
@@ -5,7 +5,12 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 space-y-6">
+        <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
+                @include('admin.elections.partials.tabs')
+            </div>
+        </div>
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="border-b border-gray-200 bg-white p-6">
@@ -36,7 +41,6 @@
                 </div>
             </div>
         </div>
-        <div class="mt-6"></div>
         <div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="border-b border-gray-200 bg-white p-6">

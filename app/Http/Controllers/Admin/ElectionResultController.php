5743769ce3bc4d96c4e45b09439e2582bae10778
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-03-26 20:49:48 +0800

    Update ElectionResultController.php

--- app/Http/Controllers/Admin/ElectionResultController.php
+++ app/Http/Controllers/Admin/ElectionResultController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Election;
+use Illuminate\Database\Eloquent\Collection as EloquentCollection;
 
 class ElectionResultController extends Controller
 {
-    public function show(Election $election)
+    public function __invoke(Election $election)
     {
-        return view('admin.elections.result')
-            ->with('winners', $election->winners);
+
+        return view('admin.elections.result', [
+            'election' => $election,
+            'hasNoWinners' => $election->winners()->doesntExist(),
+            'tiedCandidates' => $election->candidates()
+                ->withCount('votes')
+                ->get()
+                ->groupBy('position.name')
+                ->sortKeys()
+                ->map(fn (EloquentCollection $candidates) => $candidates->sortByDesc('votes_count'))
+                ->map(fn (EloquentCollection $candidates) => $candidates->filter(function ($candidate) use ($candidates) {
+                    $maxVotes = $candidates->max('votes_count');
+
+                    return $candidate->votes_count === $maxVotes;
+                }))
+                ->filter(fn (EloquentCollection $candidates) => $candidates->count() > 1),
+        ]);
     }
 }

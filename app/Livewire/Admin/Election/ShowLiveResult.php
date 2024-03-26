291dc816b2fda0375a50b4baf49b2eb7c90febab
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-03-26 20:49:34 +0800

    Update ShowLiveResult.php

--- app/Livewire/Admin/Election/ShowLiveResult.php
+++ app/Livewire/Admin/Election/ShowLiveResult.php
@@ -3,35 +3,21 @@
 namespace App\Livewire\Admin\Election;
 
 use App\Models\Election;
-use App\Models\User;
+use Livewire\Attributes\Locked;
 use Livewire\Component;
 
 class ShowLiveResult extends Component
 {
-    public $election;
+    #[Locked]
+    public Election $election;
 
-    public function mount(Election $election)
+    public function mount(Election $election): void
     {
         $this->election = $election;
     }
 
-    public function render()
+    public function render(): \Illuminate\Contracts\View\View
     {
-        return view('livewire.admin.election.show-live-result', [
-            'candidates' => $this->queryCandidates(),
-        ]);
-    }
-
-    public function queryCandidates()
-    {
-        return $this->election->candidates()
-            ->select(['position_id', 'user_id'])
-            ->with(['user.department', 'position'])
-            ->withCount('votes')
-            ->orderBy('position_id')
-            ->orderBy(User::select('name')->whereColumn('candidates.user_id', 'users.id'))
-            ->get()
-            ->groupBy('position.name')
-            ->toBase();
+        return view('livewire.admin.election.show-live-result');
     }
 }

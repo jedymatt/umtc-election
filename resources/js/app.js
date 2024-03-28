1782d966303494364a43cc746b94aa7b38c875b6
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-03-28 21:26:48 +0800

    Refactor toast alert

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,26 +3,23 @@ import Swal from 'sweetalert2';
 
 window.Swal = Swal;
 
-window.fireToast = ({icon, message}) => {
-    const Toast = Swal.mixin({
-        toast: true,
-        position: 'top-end',
-        showConfirmButton: false,
-        timer: 5000,
-        timerProgressBar: true,
-        showCloseButton: true,
-        padding: '1rem',
-    });
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-end',
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true,
+    showCloseButton: true,
+    padding: '1rem',
+});
 
-    Toast.fire({
-        icon: icon,
-        titleText: message,
-    });
-};
+const fireToast = ({icon, message}) => Toast.fire({icon: icon, titleText: message});
 
-window.addEventListener('toast-alert', function (event) {
-    fireToast({
-        icon: event.detail.type,
-        message: event.detail.message,
+window.fireToast = fireToast;
+
+window.addEventListener('toast-alert', (event) => {
+    fireToast({icon: 'error', message: 'test'});
+    window.fireToast({
+        icon: event.detail.type, message: event.detail.message,
     });
 });

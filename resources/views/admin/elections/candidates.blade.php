e1f32fca81c5b40cf719ab55780cb1e2f0fa828e
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-03-28 21:27:24 +0800

    Conditionaly show operations when not ended

--- resources/views/admin/elections/candidates.blade.php
+++ resources/views/admin/elections/candidates.blade.php
@@ -15,23 +15,6 @@
             <div class="overflow-hidden bg-white shadow-sm sm:rounded-lg">
                 <div class="border-b border-gray-200 bg-white p-6">
                     {{-- Content --}}
-                    <div class="flex justify-end">
-                        <x-primary.button-link href="{{ route('admin.elections.show', $election)}}" class="gap-2">
-                            <span>Done</span>
-                            <svg
-                                class="h-5 w-5"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M16.704 4.153a.75.75 0 01.143 1.052l-8 10.5a.75.75 0 01-1.127.075l-4.5-4.5a.75.75 0 011.06-1.06l3.894 3.893 7.48-9.817a.75.75 0 011.05-.143z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                            </x-button-primary>
-                    </div>
                     <div class="mt-4 h-full min-h-[50vh]">
                         @livewire('admin.election.manage-candidates', ['election' => $election])
                     </div>
--- resources/views/livewire/admin/election/manage-candidates.blade.php
+++ resources/views/livewire/admin/election/manage-candidates.blade.php
@@ -1,48 +1,51 @@
 <div>
-    <div class="flex flex-row">
-        <select wire:model="selectedPositionId">
-            @foreach ($positions as $position)
-                <option value="{{ $position->id }}">
-                    {{ $position->name }}
-                </option>
-            @endforeach
-        </select>
-        <div class="relative flex-grow"
-             x-data="{ open: @entangle('showResults') }"
-             x-on:click.outside="open = false"
-        >
-            <input
-                class="w-full"
-                type="search"
-                placeholder="Select a candidate"
-                wire:model.live.debounce="searchText"
-                x-on:focus="open = $wire.searchText.length > 0"
-            />
-
-            <div
-                class="absolute inset-x-0 top-0 z-20 mt-12 max-h-52 w-full overflow-clip overflow-y-scroll rounded-md border bg-white shadow-lg"
-
-                x-show="open"
-                wire:show="searchText"
-                x-cloak
+    @if(!$election->isEnded())
+        <div class="flex flex-row">
+            <select wire:model="selectedPositionId">
+                @foreach ($positions as $position)
+                    <option value="{{ $position->id }}">
+                        {{ $position->name }}
+                    </option>
+                @endforeach
+            </select>
+            <div class="relative flex-grow"
+                 x-data="{ open: @entangle('showResults') }"
+                 x-on:click.outside="open = false"
             >
-                <div class="py-1">
-                    @foreach ($results as $user)
-                        <div
-                            class="cursor-pointer px-4 py-1 hover:bg-slate-200"
-                            tabindex="1"
-                            wire:click="selectUserFromResults({{ $user->id }})"
-                        >
-                            {{ $user->name }}
-                            <div class="text-xs text-slate-800">
-                                {{ $user->email }}
+                <input
+                    class="w-full"
+                    type="search"
+                    placeholder="Select a candidate"
+                    wire:model.live.debounce="searchText"
+                    x-on:focus="open = $wire.searchText.length > 0"
+                />
+
+                <div
+                    class="absolute inset-x-0 top-0 z-20 mt-12 max-h-52 w-full overflow-clip overflow-y-scroll rounded-md border bg-white shadow-lg"
+
+                    x-show="open"
+                    wire:show="searchText"
+                    x-cloak
+                >
+                    <div class="py-1">
+                        @foreach ($results as $user)
+                            <div
+                                class="cursor-pointer px-4 py-1 hover:bg-slate-200"
+                                tabindex="1"
+                                wire:click="selectUserFromResults({{ $user->id }})"
+                            >
+                                {{ $user->name }}
+                                <div class="text-xs text-slate-800">
+                                    {{ $user->email }}
+                                </div>
                             </div>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    @endif
+
     <div>
         <table class="w-full table-auto">
             <tbody class="divide-y">
@@ -62,26 +65,29 @@
                         {{ $candidate->position->name }}
                     </td>
                     <td>
-                        <button
-                            class="inline-flex flex-row items-center justify-center gap-1 rounded-full bg-red-600 px-2 py-1 text-white shadow-sm"
-                            type="button"
-                            wire:click="removeCandidate({{ $candidate->id }})"
-                            wire:loading.attr="disabled"
-                            wire:confirm="Are you sure you want to remove this candidate? \n{{$candidate->user->name}}"
-                        >
-
-                            <span class="text-xs tracking-wide">Remove</span>
-                            <svg
-                                class="h-4 w-4"
-                                xmlns="http://www.w3.org/2000/svg"
-                                viewBox="0 0 20 20"
-                                fill="currentColor"
+                        @if(!$election->isEnded())
+                            <button
+                                class="inline-flex flex-row items-center justify-center gap-1 rounded-full bg-red-600 px-2 py-1 text-white shadow-sm"
+                                type="button"
+                                wire:click="removeCandidate({{ $candidate->id }})"
+                                wire:loading.attr="disabled"
+                                wire:confirm="Are you sure you want to remove this candidate? \n{{$candidate->user->name}}"
                             >
-                                <path
-                                    d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
-                                />
-                            </svg>
-                        </button>
+
+                                <span class="text-xs tracking-wide">Remove</span>
+                                <svg
+                                    class="h-4 w-4"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 20 20"
+                                    fill="currentColor"
+                                >
+                                    <path
+                                        d="M6.28 5.22a.75.75 0 00-1.06 1.06L8.94 10l-3.72 3.72a.75.75 0 101.06 1.06L10 11.06l3.72 3.72a.75.75 0 101.06-1.06L11.06 10l3.72-3.72a.75.75 0 00-1.06-1.06L10 8.94 6.28 5.22z"
+                                    />
+                                </svg>
+                            </button>
+
+                        @endif
                     </td>
                 </tr>
             @endforeach

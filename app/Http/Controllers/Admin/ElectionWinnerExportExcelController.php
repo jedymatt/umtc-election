ddd4db9b6d88e74cd518118fb7b1a556b8c547e4
Jedy Matt Tabasco <54795428+jedymatt@users.noreply.github.com>
2024-03-28 19:59:25 +0800

    Update ElectionWinnerExportExcelController.php

--- app/Http/Controllers/Admin/ElectionWinnerExportExcelController.php
+++ app/Http/Controllers/Admin/ElectionWinnerExportExcelController.php
@@ -5,14 +5,13 @@ namespace App\Http\Controllers\Admin;
 use App\Exports\ElectionWinnersExport;
 use App\Http\Controllers\Controller;
 use App\Models\Election;
-use Illuminate\Support\Str;
 use Maatwebsite\Excel\Facades\Excel;
 
 class ElectionWinnerExportExcelController extends Controller
 {
     public function __invoke(Election $election)
     {
-        $fileName = Str::slug($election->title).'_'.now()->timestamp.'.xlsx';
+        $fileName = 'election_'.str($election->title)->slug('_').'_'.now()->timestamp.'.xlsx';
 
         return Excel::download(new ElectionWinnersExport($election), $fileName);
     }
